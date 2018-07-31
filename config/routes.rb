98b683b4001c8a2f86469a361a11f3ab4d558ab3
Henadzi Simonovich <375295716621@yandex.by>
2018-07-31 11:28:02 +0300

    Add Rails Admin
    Entry point: http://localhost:3000/dbadmin
    Use Devise, there may be conflicts in the routes
    A configuration is made without distributing user roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/dbadmin', as: 'rails_admin'
+  devise_for :users
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
