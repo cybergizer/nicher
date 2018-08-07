df119785224ef68b93dc6b9ab3d5babcf00fd837
Marie <triangletrue@yahoo.com>
2018-08-07 10:58:48 +0300

    css -> scss & helper tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   root to: 'items#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
