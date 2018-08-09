17b326636f09d9ff2c5b6320288c4163165998bb
Sasha Shostyr <sashashostyr@gmail.com>
2018-08-09 12:06:22 +0300

    Add seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :items
+  has_many :items, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Create User'
+
+User.destroy_all
+user = User.create(
+  first_name: 'test',
+  email: 'test@mail.ru',
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Date.today
+)
+
+puts 'Create items'
+
+Item.destroy_all
+10.times do
+  Item.create(
+    title: Faker::Lorem.word,
+    description: Faker::Lorem.sentence(4, false),
+    user_id: user.id
+  )
+end
