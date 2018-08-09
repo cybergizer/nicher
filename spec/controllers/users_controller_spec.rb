5bc0dd0521b6810cca15e991f397af4b2b20b7f1
leshkacore <lechan662@gmail.com>
2018-08-09 15:45:24 +0300

    Add rspec tests: UserProfile model and UsersController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,25 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  login_user
+  let(:valid_session) { {} }
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  describe 'show action' do
+    it 'returns http success' do
+      get :show, params: { id: subject.current_user.id }, session: valid_session
+      expect(response).to be_successful
     end
   end
 
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
+  describe 'edit action' do
+    it 'returns http success' do
+      get :edit, params: { id: subject.current_user.id }, session: valid_session
+      expect(response).to be_successful
     end
   end
 
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
+  describe 'update action' do
+    let(:new_attributes) {
+      {
+      full_name: 'alex',
+      city: 'minsk',
+      description: 'test',
+      user_id: subject.current_user.id
+      }
+    }
+    it 'updates the requested user profile' do
+      put :update, params: { id: subject.current_user.id, user: { user_profile: new_attributes }}, session: valid_session
+      expect(response).to redirect_to(subject.current_user)
     end
   end
 
--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe UserProfile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(email: 'test@example.com', password: 'password123', password_confirmation: 'password123') }
+
+  it 'should be a UserProfile' do
+    user.user_profile = UserProfile.create(full_name: 'Alex', city: 'Minsk', description: 'test')
+    expect(user.user_profile).to be_kind_of(UserProfile)
+  end
+
+  it 'is not valid without a city' do
+    expect(UserProfile.create(full_name: 'Alex', description: 'test')).to_not be_valid
+  end
+
+  it 'is not valid without a full_name' do
+    expect(UserProfile.create(city: 'Minsk', description: 'test')).to_not be_valid
+  end
+
+  it 'is not valid without a description' do
+    expect(UserProfile.create(full_name: 'Alex', city: 'Minsk')).to_not be_valid
+  end
+
+  it 'is actual contained in UserProfile table' do
+    user.user_profile = UserProfile.create(full_name: 'Alex', city: 'Minsk', description: 'test')
+    expect(UserProfile.all).to contain_exactly(user.user_profile)
+  end
+
+  it 'is returns actual attributes' do
+    user.user_profile = UserProfile.create(full_name: 'Alex', city: 'Minsk', description: 'test')
+    expect(user.user_profile.full_name).to eq('Alex')
+    expect(user.user_profile.city).to eq('Minsk')
+    expect(user.user_profile.description).to eq('test')
+  end
 end
