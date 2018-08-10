33f7e6a228f2516dac6f6963889ca4b92735630b
leshkacore <lechan662@gmail.com>
2018-08-10 09:33:44 +0300

    Delete useless instance variables and use rails helpers for path to User actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,27 @@
 class UsersController < ApplicationController
   def show
-    @user = current_user
-    @user.user_profile = UserProfile.where(user_id: @user.id).first_or_create do |profile|
+    @user_profile = UserProfile.find_or_create_by(user_id: current_user.id) do |profile|
       profile.full_name = 'Some name'
-      profile.city = 'Some city'
       profile.description = 'Some description'
+      profile.city = 'Some city'
     end
   end
 
-  def edit
-    @user = current_user
-  end
+  def edit; end
 
   def update
-    @user = current_user
-    if @user.update(user_profile_params)
-      redirect_to @user
+    if current_user.update(user_profile_params)
+      redirect_to current_user
     else
       render :edit
     end
   end
 
   private
+
+  def user_profile
+    current_user.user_profile
+  end
 
   def user_profile_params
     params.require(:user).permit(user_profile_attributes: %i(full_name city description))
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :items, dependent: :destroy
   has_one :user_profile, dependent: :destroy
   accepts_nested_attributes_for :user_profile, allow_destroy: true
->>>>>>> Add user profile to project
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
