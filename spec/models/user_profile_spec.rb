0d918cca1d7a654b8478ccb7501974da7da17ebe
leshkacore <lechan662@gmail.com>
2018-08-16 08:39:30 +0300

    Move tests for AvatarUploader to another file

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'carrierwave/test/matchers'
 
 RSpec.describe UserProfile, type: :model do
   let(:user) { FactoryBot.create(:user) }
@@ -29,39 +28,5 @@ RSpec.describe UserProfile, type: :model do
     expect(user.user_profile.full_name).to eq('Alex')
     expect(user.user_profile.city).to eq('Minsk')
     expect(user.user_profile.description).to eq('test')
-  end
-end
-
-RSpec.describe AvatarUploader, type: :uploader do
-  include CarrierWave::Test::Matchers
-  let(:user_profile) { FactoryBot.build(:user_profile) }
-  let(:uploader) { AvatarUploader.new(user_profile, :avatar) }
-
-  before do
-    AvatarUploader.enable_processing = true
-    File.open(File.join(Rails.root,'spec/fixtures/files/avatar.jpg')) { |f| uploader.store!(f) }
-  end
-
-  after do
-    AvatarUploader.enable_processing = false
-    uploader.remove!
-  end
-
-  context 'the versions' do
-    it 'scales down a landscape image to be exactly 250 by 250 pixels' do
-      expect(uploader.thumb).to have_dimensions(250, 250)
-    end
-
-    it 'scales down a landscape image to be exactly 250 by 250 pixels' do
-      expect(uploader.icon).to have_dimensions(50, 50)
-    end
-  end
-
-  it 'has the correct format' do
-    expect(uploader).to be_format('JPEG')
-  end
-
-  it 'has the correct size' do
-    expect(uploader.size).to be <=1000000 # bytes
   end
 end
