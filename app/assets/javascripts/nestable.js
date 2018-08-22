b0b5b77d2d199d35a3ad6006554059b666bd10d0
Vadser <vadser1999@gmail.com>
2018-08-22 10:49:30 +0300

    Add id of privious parent for niche and category

--- app/assets/javascripts/nestable.js
+++ app/assets/javascripts/nestable.js
@@ -3,7 +3,8 @@ $(document).ready(function () {
     afterInit: function(){
       $('.dd').find('.dd-item').map(function(_, el){ 
         if ($(el).find('.dd-list .dd-item').length == 0) {
-          $(el).children('button').hide();
+          $(el).find('button[data-action="expand"]').hide();
+          $(el).find('button[data-action="collapse"]').hide();
         } else {
           $(el).find('button[data-action="expand"]').show();
           $(el).find('button[data-action="collapse"]').hide();
@@ -14,12 +15,17 @@ $(document).ready(function () {
 
   $('.dd').on('dragEnd', function(event, item, source, destination, position) {
     var currentItem = $(item).attr('data-id');
-    var itemParent = $(item).parent().parent().attr('data-id');
+    var result = $(item).parent().parent().attr('data-id')
+    var itemParent = (typeof result === "undefined" ? "": result);
     var typecheck = $(this).hasClass('niches');
-    if(typecheck) {
-      moveElements('niches', currentItem, itemParent);
-    }else{
-      moveElements('categories', currentItem, itemParent);
+    var priviousParent = $(item).attr('data-parent')
+    if (priviousParent != itemParent){
+      $(item).attr('data-parent', itemParent);
+      if(typecheck) {
+        moveElements('niches', currentItem, itemParent);
+      }else{
+        moveElements('categories', currentItem, itemParent);
+      }
     }
   });
   
