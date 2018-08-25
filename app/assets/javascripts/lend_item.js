72f9cc04a011aa25bfffb01767a11c670897515c
Vadser <vadser1999@gmail.com>
2018-08-25 22:53:37 +0300

    Fix js for lend items

--- app/assets/javascripts/lend_item.js
+++ app/assets/javascripts/lend_item.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         addStylesToDialog();
         fetchRentFormContent(id);
       },
-      close: addStylesToDialog
+      close: closeRentItemDialog
     });
   });
 
