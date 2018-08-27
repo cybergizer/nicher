cef98b393391cb50d66e2cec2a7147afb06bba6e
leshkacore <lechan662@gmail.com>
2018-08-27 09:02:38 +0300

    Add edit view for UserProfile

--- app/assets/javascripts/user_profile.js
+++ app/assets/javascripts/user_profile.js
@@ -1,5 +1,25 @@
-$(function() {
+$(document).ready(function() {
   $('.birthday_picker').datepicker({
     dateFormat: 'yy-mm-dd'
+  });
+  function readURL(input) {
+
+    if (input.files && input.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function(e) {
+                  $('#pre-load').attr('src', e.target.result);
+
+          }
+
+              reader.readAsDataURL(input.files[0]);
+
+    }
+
+  }
+
+  $("#imgInp").change(function() {
+      readURL(this);
+
   });
 });
