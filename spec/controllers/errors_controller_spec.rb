9137c7c66eed21758a3e57fe0e7f0c21aa18d438
Vadser <vadser1999@gmail.com>
2018-08-27 18:13:38 +0300

    Add tests for errors controller

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe ErrorsController, type: :controller do
+  let(:user) { create(:user) }
 
+  before do
+    sign_in user
+  end
+
+  describe 'GET #not_found' do
+    before do
+      get :not_found
+    end
+
+    it 'returns http 404' do
+      expect(response).to have_http_status('404')
+    end
+
+    it 'renders the not_found view' do
+      expect(response).to render_template :not_found
+    end
+  end
+
+  describe 'GET #internal_server_error' do
+    before do
+      get :internal_server_error
+    end
+
+    it 'returns http 500' do
+      expect(response).to have_http_status('500')
+    end
+
+    it 'renders the internal_server_error view' do
+      expect(response).to render_template :internal_server_error
+    end
+  end
 end
