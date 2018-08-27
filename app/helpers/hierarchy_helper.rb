312ffe016c098df2bdb61b8b9d036bdccaa8c79c
Vadser <vadser1999@gmail.com>
2018-08-27 10:30:03 +0300

    Add breadcrumbs for niches and categories

--- app/helpers/hierarchy_helper.rb
+++ app/helpers/hierarchy_helper.rb
@@ -10,6 +10,12 @@ module HierarchyHelper
     end
   end
 
+  def breadcrumb_output(object)
+    object.ancestors.each_with_object('') do |object, output|
+      output << "<li><a href=#{object.id}>#{object.name}</a></li>"
+    end
+  end
+
   private
 
   def children_output(children, output, template)
