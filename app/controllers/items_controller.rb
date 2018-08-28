33d66aef8f075f07ab22404bbe3bf9ed475e294d
Marie Akhramenko <triangletrue@yahoo.com>
2018-08-28 14:22:57 +0300

    Add base for timeline

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,7 @@ class ItemsController < ApplicationController
 
   def index; end
 
-  def show
-    @versions = @item.versions.map(&:reify)[1..-1]
-  end
+  def show; end
 
   def new
     @item = current_user.items.build
