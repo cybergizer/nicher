cefb207e00f2b4130a48a64dd11698324d553d8a
Vadser <vadser1999@gmail.com>
2018-08-29 14:45:24 +0300

    Remove call of include from niche show view, fix schema.rb

--- app/controllers/niches_controller.rb
+++ app/controllers/niches_controller.rb
@@ -75,6 +75,11 @@ class NichesController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_niche
     @niche = set_resource
+    set_niche_items
+  end
+
+  def set_niche_items
+    @items = @niche.items.includes(:category)
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
