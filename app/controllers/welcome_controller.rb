551fca86e0b2a2d3bbbbd30ad7aa421b24f328ca
Vadser <vadser1999@gmail.com>
2018-08-30 17:12:57 +0300

    Fix bullet error for welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @items = Item.free.search(params[:search]).includes(:rent_item, :free_item_request)
+    @items = Item.free.search(params[:search])
+    @items = @items.includes(:rent_item, :free_item_request) if current_user
   end
 end
