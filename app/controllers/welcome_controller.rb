be042ee01424beeffce55d7ad0404b58330badeb
Marie Akhramenko <triangletrue@yahoo.com>
2018-09-01 13:13:53 +0300

    Make style fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,15 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :set_items, only: :index
 
-  def index
-    @items = Item.free.search(params[:search]).sample(8)
-    set_items if current_user
-  end
+  def index; end
 
   def set_items
+    @items = Item.free.search(params[:search]).sample(8)
+    set_items_for_user if current_user
+  end
+
+  def set_items_for_user
     @items = Item.free.includes(:rent_item, :free_item_request)
     @items = current_user.items.search(params[:search]).includes(:rent_item, :free_item_request) if params[:search]
   end
